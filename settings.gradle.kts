acb7e784ab89cb3ee80bc54b9f46dcdce9832234
crowem3w <crow.em3w@gmail.com>
2026-09-04 05:01:38 +0000

    v4v1a

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "io.objectbox") {
+                useModule("io.objectbox:objectbox-gradle-plugin:${requested.version}")
+            }
+        }
+    }
 }
 
 dependencyResolutionManagement {
