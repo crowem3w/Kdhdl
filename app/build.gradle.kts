8e26ef1647cd30cf796ee18adf81950007b68571
crowem3w <crow.em3w@gmail.com>
2026-09-10 11:52:36 +0000

    sl5-10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "apcka"
+        applicationId = "com.apcka"
         minSdk = 30
         targetSdk = 37
         versionCode = 1
